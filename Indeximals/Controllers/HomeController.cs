0ff654584ce2a2d1ce508ed00e063d21f51a29fa
Dan Butvinik <butvinik@outlook.com>
2017-11-29 15:45:23 +0000

    sets up storage intake form (#1)

---
+++ Indeximals/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Indeximals.Models;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
+using Microsoft.WindowsAzure.Storage;
+using System.Linq;
+
+namespace Indeximals.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CloudBlobClient _cloudBlobClient;
+        private readonly CloudBlobContainer _animalsContainer;
+
+        public HomeController(CloudBlobClient cloudBlobClient)
+        {
+            _cloudBlobClient = cloudBlobClient;
+            _animalsContainer = _cloudBlobClient.GetContainerReference("animals");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create(Animal model)
+        {
+            if(model?.Id != null)
+            {
+                await _animalsContainer.CreateIfNotExistsAsync();
+
+                var blob = _animalsContainer.GetBlockBlobReference($"{model.Id}.json");
+                blob.Properties.ContentType = "application/json";
+                await blob.UploadTextAsync(JsonConvert.SerializeObject(model));
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> List()
+        {
+            await _animalsContainer.CreateIfNotExistsAsync();
+            var animalBlobs = await _animalsContainer.ListBlobsSegmentedAsync(null);
+
+            var animalJsons = await Task.WhenAll(animalBlobs.Results
+                .OfType<CloudBlockBlob>()
+                .Select(x => x.DownloadTextAsync()));
+
+            return View(animalJsons
+                .Select(x => JsonConvert.DeserializeObject<Animal>(x))
+                .ToArray());
+        }
+
+        public async Task<IActionResult> Query()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Indeximals/Models/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Indeximals.Models
+{
+    public class Animal
+    {
+        [Required]
+        [Display(Name = "Id")]
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Sound")]
+        public string Sound { get; set; }
+
+        [Required]
+        [Display(Name = "Diet")]
+        public Diet Diet { get; set; }
+    }
+}
